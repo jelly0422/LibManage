349ff24aa942725e70819b76bb4d6a9242938265
陈国栋 <1187893119@qq.com>
2021-08-11 20:35:04 +0800

    ..

--- src/main/java/com/library/manage/ManageApplication.java
+++ src/main/java/com/library/manage/ManageApplication.java
@@ -2,6 +2,7 @@ package com.library.manage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.library.manage")
 public class ManageApplication {
