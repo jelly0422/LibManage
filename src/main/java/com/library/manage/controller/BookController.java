231d4364b5a76825795bf6d5964dabd3d9606dab
陈国栋 <1187893119@qq.com>
2021-08-11 20:34:17 +0800

    热门书籍获取方式修改

--- src/main/java/com/library/manage/controller/BookController.java
+++ src/main/java/com/library/manage/controller/BookController.java
@@ -1,16 +1,24 @@
 package com.library.manage.controller;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 import com.library.manage.model.support.BaseResponse;
 import com.library.manage.model.vo.BookVO;
 import com.library.manage.service.BookService;
 import io.swagger.annotations.ApiOperation;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/book")
@@ -18,6 +26,8 @@ public class BookController {
 
     @Autowired
     BookService bookService;
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
 
     @ApiOperation("获取所有书籍以及评论和分类")
     @GetMapping("/get")
@@ -34,6 +44,17 @@ public class BookController {
     @ApiOperation("获取热门")
     @GetMapping("/gethot")
     public BaseResponse<List<BookVO>> getHot(){
-        return BaseResponse.ok(bookService.getHotBooks());
+        List<BookVO> hot = null;
+        ListOperations<String, Object> opsForList = redisTemplate.opsForList();
+        Long size;
+        if (redisTemplate.hasKey("hot") && (size = opsForList.size("hot")) > 0){
+            hot = new ArrayList<>();
+            List<Object> range = opsForList.range("hot", 0, size - 1);
+//            System.out.println(range);
+            hot = range.stream().map(o -> (BookVO)o).collect(Collectors.toList());
+        }else{
+            hot = bookService.getHotBooks();
+        }
+        return BaseResponse.ok(hot);
     }
 }
