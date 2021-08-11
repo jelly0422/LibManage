4033e933d1ac40f82abe30f110d64ab11526e375
陈国栋 <1187893119@qq.com>
2021-08-11 20:41:18 +0800

    注释增加

--- src/main/java/com/library/manage/dao/CategoryDao.java
+++ src/main/java/com/library/manage/dao/CategoryDao.java
@@ -3,5 +3,12 @@ package com.library.manage.dao;
 import com.library.manage.model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryDao extends JpaRepository<Category, String> {
+import java.util.Optional;
+
+/**
+ * @author jelly
+ */
+public interface CategoryDao extends JpaRepository<Category, Integer> {
+
+    Optional<Category> findCategoryByCode(String code);
 }
